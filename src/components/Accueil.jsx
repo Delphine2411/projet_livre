3fc66beaf69c231e808280e49c844830d11636b2
Delphine2411 <kpankpand@gmail.com>
2025-04-10 14:14:04 +0100

    nav

---
+++ src/components/Accueil.jsx
@@ -0,0 +1,13 @@
+
+function Accueil() {
+  
+
+  return (
+    <div>
+       <h1 className='text-danger'>Bienvenue sur notre page</h1>
+       <img src="" alt="" />
+    </div>
+  );
+}
+
+export default Accueil;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/NavBar'
+import App from './App'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+
     <NavBar />
+    <App />
+    
   </StrictMode>,
 )
